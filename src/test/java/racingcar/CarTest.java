4837a54b49ce2e0479a3ea7a7d9faa3442854aff
eastman22 <eastman@tmapmobility.com>
2022-10-08 15:40:51 +0900

    test(자동차-전진/정지): 레이싱숫자에 따라 전진/정지를 모두 검증하도록 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,10 @@
 package racingcar;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
@@ -18,22 +22,18 @@ public class CarTest {
         racingNumberGenerator.close();
     }
 
-
-    @DisplayName("전진_위치이동_검증")
-    @Test
-    void 전진_위치이동_검증() {
+    @ParameterizedTest(name = "레이스 후 전진/정지 위치 검증 - 레이싱번호: {0}, 위치: {1}")
+    @CsvSource(value = {"4:1", "3:0"}, delimiter = ':')
+    void 레이스_후_전진_정지_위치_검증(int racingNumber, int expectedPosition) {
         // given
         Mockito.when(RacingNumberGenerator.generate())
-                .thenReturn(4);
-
+                .thenReturn(racingNumber);
         Car car = new Car();
 
         // when
         car.race();
 
         // then
-        Assertions.assertEquals(1, car.getPosition());
+        Assertions.assertEquals(expectedPosition, car.getPosition());
     }
-
-
 }
