a20f1fdfd0aa9b3ab47841af3b61505a241d771e
eastman22 <eastman@tmapmobility.com>
2022-10-08 16:06:49 +0900

    feat(자동차 이름): 자동차 생성 시 이름 5자 이하 검증로직 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,7 +4,19 @@ public class Car {
 
     public static final int MIN_RACING_NUMBER_FOR_MOVE_FORWARD = 4;
 
+    private final String name;
     private int position;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
 
     public void race() {
         int racingNumber = RacingNumberGenerator.generate();
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,8 +1,6 @@
 package racingcar;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.MockedStatic;
@@ -22,13 +20,23 @@ public class CarTest {
         racingNumberGenerator.close();
     }
 
+    @DisplayName("이름_5자_초과_예외_발생_검증")
+    @Test
+    void 이름_5자_초과_예외_발생_검증() {
+        // given
+        String inputName = "lovelyCar";
+
+        // when, then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Car(inputName));
+    }
+
     @ParameterizedTest(name = "레이스 후 전진/정지 위치 검증 - 레이싱번호: {0}, 위치: {1}")
     @CsvSource(value = {"4:1", "3:0"}, delimiter = ':')
     void 레이스_후_전진_정지_위치_검증(int racingNumber, int expectedPosition) {
         // given
         Mockito.when(RacingNumberGenerator.generate())
                 .thenReturn(racingNumber);
-        Car car = new Car();
+        Car car = new Car("myCar");
 
         // when
         car.race();
