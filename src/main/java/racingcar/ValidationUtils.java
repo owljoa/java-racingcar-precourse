dee0189e6fe7deb31f4389bb2e86367ee058d973
eastman22 <eastman@tmapmobility.com>
2022-10-09 18:33:21 +0900

    feat(사용자입력 예외): 시도할 횟수 크기 검증 기능 추가

--- src/main/java/racingcar/ValidationUtils.java
+++ src/main/java/racingcar/ValidationUtils.java
@@ -1,10 +1,11 @@
 package racingcar;
 
 public class ValidationUtils {
-
+    private static final int MIN_TARGET_RACE_COUNT = 1;
 
     public static void validateRaceCount(String inputRaceCount) {
         validateIfInputIsInteger(inputRaceCount);
+        validateIfLessThanMinimumRaceCount(Integer.parseInt(inputRaceCount));
     }
 
     private static void validateIfInputIsInteger(String input) {
@@ -12,6 +13,12 @@ public class ValidationUtils {
             Integer.parseInt(input);
         } catch (NumberFormatException nfe) {
             throw new IllegalArgumentException("시도할 횟수는 숫자만 입력이 가능합니다.");
+        }
+    }
+
+    private static void validateIfLessThanMinimumRaceCount(int raceCount) {
+        if (raceCount < MIN_TARGET_RACE_COUNT) {
+            throw new IllegalArgumentException("레이스 횟수는 최소 1회입니다.");
         }
     }
 }
--- src/test/java/racingcar/ValidationUtilsTest.java
+++ src/test/java/racingcar/ValidationUtilsTest.java
@@ -13,4 +13,13 @@ public class ValidationUtilsTest {
         // when, then
         Assertions.assertThrows(IllegalArgumentException.class, () -> ValidationUtils.validateRaceCount(inputRaceCount));
     }
+
+    @Test
+    void 레이스_횟수는_1보다_큰값이어야_처리_가능() {
+        // given
+        String inputRaceCount = "0";
+
+        // when, then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> ValidationUtils.validateRaceCount(inputRaceCount));
+    }
 }
