639e73d7a1865fb791ec15ecba3e81a0b226d12b
eastman22 <eastman@tmapmobility.com>
2022-10-09 17:50:07 +0900

    feat(사용자입력): 사용자로부터 자동차 이름들을 입력받아 자동차 컬렉션 객체 생성

---
+++ src/main/java/racingcar/CommandLineInterface.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CommandLineInterface {
+
+    public static final String COMMA = ",";
+    public static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
+
+    public void startGame() {
+        Cars cars = null;
+
+        while (cars == null) {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String[] carNames = readCarNames();
+            cars = makeCars(carNames);
+        }
+    }
+
+    private String[] readCarNames() {
+        String inputCarNames = Console.readLine();
+        return inputCarNames.split(COMMA);
+    }
+
+    private Cars makeCars(String[] carNames) {
+        try {
+            return new Cars(carNames);
+        } catch (IllegalArgumentException iae) {
+            System.out.println(ERROR_MESSAGE_PREFIX + iae.getMessage());
+            return null;
+        }
+    }
+}
