d52066aa5388649a30329c7e6bddf1fc8cbe7f93
eastman22 <eastman@tmapmobility.com>
2022-10-09 18:46:11 +0900

    feat(사용자입력): 레이스 횟수 입력받아 레이스 진행하도록 사용자입력 기능 추가

--- src/main/java/racingcar/GameUserInterface.java
+++ src/main/java/racingcar/GameUserInterface.java
@@ -8,13 +8,27 @@ public class GameUserInterface {
     public static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
 
     public void startGame() {
+        Game game;
         Cars cars = null;
+        int targetRaceCount = 0;
 
         while (cars == null) {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String[] carNames = readCarNames();
             cars = makeCars(carNames);
         }
+        game = new Game(cars);
+
+        while (targetRaceCount <= 0) {
+            System.out.println("시도할 회수는 몇회인가요?");
+            targetRaceCount = readRaceCount();
+        }
+
+        for (int round = 0; round < targetRaceCount; round++) {
+            game.startRace();
+            System.out.println(game.getResultMessage());
+        }
+        System.out.println(game.getWinnerMessage());
     }
 
     private String[] readCarNames() {
@@ -28,6 +42,17 @@ public class GameUserInterface {
         } catch (IllegalArgumentException iae) {
             System.out.println(ERROR_MESSAGE_PREFIX + iae.getMessage());
             return null;
+        }
+    }
+
+    private int readRaceCount() {
+        try {
+            String inputRaceCount = Console.readLine();
+            ValidationUtils.validateRaceCount(inputRaceCount);
+            return Integer.parseInt(inputRaceCount);
+        } catch (IllegalArgumentException iae) {
+            System.out.println(ERROR_MESSAGE_PREFIX + iae.getMessage());
+            return 0;
         }
     }
 }
